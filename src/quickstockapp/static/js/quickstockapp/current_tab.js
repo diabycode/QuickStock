a1965fcee8e33c59c681e29d76067506362be404
diabycode <diabyissouf149@gmail.com>
2024-06-25 07:36:05 +0000

    Period changed js delete; product list edited ; table.css edited ; current tab js edited ;

--- src/quickstockapp/static/js/quickstockapp/current_tab.js
+++ src/quickstockapp/static/js/quickstockapp/current_tab.js
@@ -1,8 +1,11 @@
 const currentTab = document.getElementById("current_tab")
 if (currentTab) {
     const menu = Array.from(document.querySelectorAll(".sidebar-content .menu li"))
-    if (currentTab.value == "dashbord") menu[0].classList.add("current");  
-    if (currentTab.value == "products") menu[1].classList.add("current");
-    if (currentTab.value == "orders") menu[2].classList.add("current");
-    if (currentTab.value == "sales") menu[3].classList.add("current");
+    menu.forEach(li => {
+        const a = li.querySelector("a");
+        if (a.href.includes(currentTab.value)) li.classList.add("current");  
+        else if (a.href.includes(currentTab.value)) li.classList.add("current");  
+        else if (a.href.includes(currentTab.value)) li.classList.add("current");  
+        else if (a.href.includes(currentTab.value)) li.classList.add("current");  
+    })
 }
--- src/quickstockapp/static/js/quickstockapp/period_change.js
+++ src/quickstockapp/static/js/quickstockapp/period_change.js
@@ -1,4 +1,4 @@
-document.querySelector("#period").addEventListener("change", (e) => {
-    e.target.closest("form").submit()
-    // document.querySelector("#submit_period").click();
-})
+// document.querySelector("#period").addEventListener("change", (e) => {
+//     e.target.closest("form").submit()
+//     // document.querySelector("#submit_period").click();
+// })
